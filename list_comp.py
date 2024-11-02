30902c0001eef2bc6afe86d9dd138c542c2b742f
humacan <132790446+humacan@users.noreply.github.com>
2024-11-02 18:48:05 +0000

    merhaba

--- list_comp.py
+++ list_comp.py
@@ -8,10 +8,9 @@ liste=[ x for x in range(10) if x % 3==0]
 liste2=[x for x in listeler.muhtelif if x.isalpha()]
 # print(liste2)
 
-
 liste3=[(listeler.ogrenciler[x],listeler.notlar[x]) for x in range (0,len(listeler.ogrenciler))]
 
-liste_dic={key:value for (key,value) in liste3}
+liste_dic={key:value for (key,value) in liste3 if value>=50}
 
 print(liste_dic)
 
